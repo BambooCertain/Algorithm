01f76386356857f2cfa3b5b65e5f97b505a781f3
张传 <61621622+BambooCertain@users.noreply.github.com>
2020-02-29 22:40:49 +0800

    Update Kruskal.cpp

--- Kruskal构造最小生成树/Kruskal.cpp
+++ Kruskal构造最小生成树/Kruskal.cpp
@@ -27,17 +27,17 @@ int n, m;
 struct node
 {
 	int u, v, val;
-}edge[maxn],ansedge[maxn];										//edge存边的端点u,v,以及边的权值val,ansedge维护构成最小生成树的边的信息
+}edge[maxn],ansedge[maxn];								//edge存边的端点u,v,以及边的权值val,ansedge维护构成最小生成树的边的信息
 int  father[maxn];
-int cmp(const node& a, const node& b)									//对于边的权值大小进行排序	
+int cmp(const node& a, const node& b)							//对于边的权值大小进行排序	
 {
 	return a.val < b.val;
 }
-int find(int x)												//通过并查集算法来判断两个顶点是不是同属于同一棵树，可以查找这两个顶点的祖先是否相同，如果相同则说明两个顶点在同一棵树中
+int find(int x)										//通过并查集算法来判断两个顶点是不是同属于同一棵树，可以查找这两个顶点的祖先是否相同，如果相同则说明两个顶点在同一棵树中
 {
 	return x == father[x] ? x : father[x] = find(father[x]);
 }
-void MergeTree(int x, int y)										// 对于不同的两个，通过MergeTree来讲两个树合并成一棵树
+void MergeTree(int x, int y)								// 对于不同的两个，通过MergeTree来讲两个树合并成一棵树
 {
 	int fx = find(x) , fy = find(y);
 	father[fx] = fy;
@@ -50,22 +50,22 @@ int main()
 	{
 		scanf("%d %d %d", &edge[i].u, &edge[i].v, &edge[i].val);
 	}
-	sort(edge + 1, edge + 1 + m, cmp);								//对边的长度进行排序，在构造最小生成树中的过程中，权值较小的边其优先级总是高于权值大的边
+	sort(edge + 1, edge + 1 + m, cmp);						//对边的长度进行排序，在构造最小生成树中的过程中，权值较小的边其优先级总是高于权值大的边
 	for (int i = 1; i <= n; ++i)
 	{
 		father[i] = i;
 	}
-	int cnt = 0;											//cnt记录用于构成最小生成树的边数，根据最小生成树的概念可知，满足条件的边数应该为顶点数-1
-	int ans = 0;											//ans记录最小生成树的权值和
+	int cnt = 0;									//cnt记录用于构成最小生成树的边数，根据最小生成树的概念可知，满足条件的边数应该为顶点数-1
+	int ans = 0;									//ans记录最小生成树的权值和
 	for (int i = 1; i <= m; ++i)
 	{
-		if (cnt == n - 1) break;								//如果构成最小生成树的边数已经为顶点数-1，那么最小生成树已经构建完成
-		if (find(edge[i].u) != find(edge[i].v))							//判断一条边的两个顶点是否在同一棵树中，如果在同一棵树中则该边为无效边，不能用于构造最小生成树
+		if (cnt == n - 1) break;						//如果构成最小生成树的边数已经为顶点数-1，那么最小生成树已经构建完成
+		if (find(edge[i].u) != find(edge[i].v))					//判断一条边的两个顶点是否在同一棵树中，如果在同一棵树中则该边为无效边，不能用于构造最小生成树
 		{
-			cnt++;										//记录用于构成最小生成树的边数
-			MergeTree(edge[i].u, edge[i].v);						//因为该边将两个不同的树进行了连接所以需要合并成一颗树
-			ans += edge[i].val;								//维护最小生成树的权值和
-			ansedge[cnt] = edge[i];								//维护构成最小生成树的边的信息
+			cnt++;								//记录用于构成最小生成树的边数
+			MergeTree(edge[i].u, edge[i].v);				//因为该边将两个不同的树进行了连接所以需要合并成一颗树
+			ans += edge[i].val;						//维护最小生成树的权值和
+			ansedge[cnt] = edge[i];						//维护构成最小生成树的边的信息
 		}
 	}
 	printf("最小生成树的权值和为：%d\n", ans);
